14e71df1be8e35fc1dec65d86fdbf65cb75f46d1
Bongani Masango <bkmasango26@gmail.com>
2025-03-31 20:21:02 +0200

    Seeding Classifieds Data

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,14 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 import { seedTaxonomy } from './taxonomy.seed';
+import { seedClassifieds } from './classified.seed';
 // import { seedAdmin } from "./admin.seed";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.$executeRaw`TRUNCATE TABLE "makes" RESTART IDENTITY CASCADE`;
+  //   await prisma.$executeRaw`TRUNCATE TABLE "makes" RESTART IDENTITY CASCADE`;
   // await prisma.$executeRaw`TRUNCATE TABLE "classifieds" RESTART IDENTITY CASCADE`;
-  await seedTaxonomy(prisma);
-  // await seedClassifieds(prisma);
+  //   await seedTaxonomy(prisma);
+  await seedClassifieds(prisma);
   // await seedImages(prisma);
   // await seedAdmin(prisma);
   // await seedCustomers(prisma);
