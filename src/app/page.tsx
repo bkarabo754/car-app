2ec9b4f183bf70a7161dc35d217748f539f929b0
Bongani Masango <bkmasango26@gmail.com>
2025-04-05 18:15:51 +0200

    Building the Homepage

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold">Welcome to My Next.js App</h1>
-    </div>
-  );
-}
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,6 +3,11 @@ import { routes } from './routes';
 export const imageSources = {
   classifiedPlaceholder:
     'https://majestic-motors.s3.us-east-1.amazonaws.com/uploads/classified-placeholder+(1).jpeg',
+
+  carLineup:
+    'https://majestic-motors.s3.us-east-1.amazonaws.com/uploads/carsLineup.jpeg',
+  featureSection:
+    'https://majestic-motors.s3.us-east-1.amazonaws.com/uploads/istockphoto-1413498884-612x612.jpeg',
 };
 
 export const CLASSIFIEDS_PER_PAGE = 3;
